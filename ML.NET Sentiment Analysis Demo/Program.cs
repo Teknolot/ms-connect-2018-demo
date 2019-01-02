fd2c8d21eef5bc3325fccbc49717a3133d77ef87
Bilgin Ercument BILTEKIN <biltekin@gmail.com>
2019-01-02 14:30:59 +0300

    ML.NET Sentiment Analysis Demo

---
+++ ML.NET Sentiment Analysis Demo/Program.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.ML;
+using Microsoft.ML.Core.Data;
+using Microsoft.ML.Runtime.Api;
+using Microsoft.ML.Runtime.Data;
+using Microsoft.ML.Transforms.Text;
+
+namespace ML.NET_Sentiment
+{
+    class Program
+    {
+
+        static readonly string _trainDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "sentiment_data.tsv");
+        static readonly string _testDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "sentiment_test.tsv");
+        static readonly string _modelPath = Path.Combine(Environment.CurrentDirectory, "Data", "Model.zip");
+        static TextLoader _textLoader;
+
+
+        static void Main(string[] args)
+        {
+            MLContext mlContext = new MLContext(seed: 0);
+
+            _textLoader = mlContext.Data.TextReader(new TextLoader.Arguments()
+            {
+                Separator = "tab",
+                HasHeader = true,
+                Column = new[]
+                {
+                    new TextLoader.Column("Label", DataKind.Bool, 0),
+                    new TextLoader.Column("Tweet", DataKind.Text, 1)
+                }
+            });
+
+            var model = Train(mlContext, _trainDataPath);
+
+            Evaluate(mlContext, model);
+
+            Predict(mlContext, model);
+
+            PredictWithModelLoadedFromFile(mlContext);
+
+        }
+
+        public static ITransformer Train(MLContext mlContext, string dataPath)
+        {
+            IDataView dataView = _textLoader.Read(dataPath);
+
+            var pipeline = mlContext.Transforms.Text.FeaturizeText("Tweet", "Features")
+
+            .Append(mlContext.BinaryClassification.Trainers.FastTree(numLeaves: 50, numTrees: 50, minDatapointsInLeaves: 20));
+
+
+            Console.WriteLine("=============== Create and Train the Model ===============");
+            var model = pipeline.Fit(dataView);
+            Console.WriteLine("=============== End of training ===============");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            return model;
+
+        }
+
+
+        public static void Evaluate(MLContext mlContext, ITransformer model)
+        {
+            IDataView dataView = _textLoader.Read(_testDataPath);
+            Console.WriteLine("=============== Evaluating Model accuracy with Test data===============");
+
+            var predictions = model.Transform(dataView);
+
+            var metrics = mlContext.BinaryClassification.Evaluate(predictions, "Label");
+
+            Console.WriteLine();
+            Console.WriteLine("Model quality metrics evaluation");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine($"Accuracy: {metrics.Accuracy:P2}");
+            Console.WriteLine($"Auc: {metrics.Auc:P2}");
+            Console.WriteLine($"F1Score: {metrics.F1Score:P2}");
+            Console.WriteLine("=============== End of model evaluation ===============");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            SaveModelAsFile(mlContext, model);
+
+        }
+
+
+        private static void SaveModelAsFile(MLContext mlContext, ITransformer model)
+        {
+            using (var fs = new FileStream(_modelPath, FileMode.Create, FileAccess.Write, FileShare.Write))
+                mlContext.Model.Save(model, fs);
+
+            Console.WriteLine("The model is saved to {0}", _modelPath);
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+        }
+
+
+        private static void Predict(MLContext mlContext, ITransformer model)
+        {
+
+            var predictionFunction = model.MakePredictionFunction<SentimentData, SentimentPrediction>(mlContext);
+
+            SentimentData sampleStatement = new SentimentData
+            {
+                Tweet = "5 gün önce açtırdığım itiraz kaydıma 2 gün içinde cevap dönüceği belirtildiği halde dönülmemiştir. 15 dk dır hatta bekletiliyorum. Acil destek alabilirmiyim?"
+            };
+
+
+            var resultprediction = predictionFunction.Predict(sampleStatement);
+
+
+
+            Console.WriteLine();
+            Console.WriteLine("=============== Prediction Test of model with a single sample and test dataset ===============");
+
+            Console.WriteLine();
+            Console.WriteLine($"Sentiment: {sampleStatement.Tweet} | Prediction: {(Convert.ToBoolean(resultprediction.Prediction) ? "Olumsuz " : "Olumlu ")} | Probability: {resultprediction.Probability} ");
+            Console.WriteLine("=============== End of Predictions ===============");
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+
+
+
+        }
+
+
+        public static void PredictWithModelLoadedFromFile(MLContext mlContext)
+        {
+            IEnumerable<SentimentData> sentiments = new[]
+            {
+                new SentimentData
+                {
+
+                    Tweet = "Bankanızdan kullanmış oldugum kredi kartıma yıllık aidat ücreti 103tl yansıtıldı geçen ay ödedim iadesini istedim ama talebim reddedildi nedeninini halen anlamış degilim ve tekrar iadesini istiyorum"
+                },
+                new SentimentData
+                {
+                    Tweet = "Bana mükemmel bir bankacılık deneyimi yaşattığın için sana çok teşekkür ederim akbank"
+                },
+                new SentimentData
+                {
+                    Tweet = "Artık cidden akbank tan nefret ettim ya günde 50kere aranmaz."
+                },
+                new SentimentData
+                {
+                    Tweet = "kodumun gerizekalı akbank robotu müşteri temsilcisine bağla"
+                },
+                new SentimentData
+                {
+                    Tweet = "Akbank reklamlarına bayılıyorum çok çok iyi"
+                }
+            };
+
+
+            ITransformer loadedModel;
+            using (var stream = new FileStream(_modelPath, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                loadedModel = mlContext.Model.Load(stream);
+            }
+
+
+
+            // Create prediction engine
+            var sentimentStreamingDataView = mlContext.CreateStreamingDataView(sentiments);
+            var predictions = loadedModel.Transform(sentimentStreamingDataView);
+
+            // Use the model to predict whether comment data is toxic (1) or nice (0).
+            var predictedResults = predictions.AsEnumerable<SentimentPrediction>(mlContext, reuseRowObject: false);
+
+            Console.WriteLine();
+
+            Console.WriteLine("=============== Prediction Test of loaded model with a multiple samples ===============");
+
+            var sentimentsAndPredictions = sentiments.Zip(predictedResults, (sentiment, prediction) => (sentiment, prediction));
+
+
+            foreach (var item in sentimentsAndPredictions)
+            {
+                Console.WriteLine($"Sentiment: {item.sentiment.Tweet} | Prediction: {(Convert.ToBoolean(item.prediction.Prediction) ? "Toxic" : "Not Toxic")} | Probability: {item.prediction.Probability} ");
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+
+            }
+            Console.WriteLine("=============== End of predictions ===============");
+
+            Console.ReadLine();
+
+
+
+        }
+
+
+
+
+    }
+}
---
+++ ML.NET Sentiment Analysis Demo/SentimentData.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.ML.Runtime.Api;
+
+namespace ML.NET_Sentiment
+{
+    public class SentimentData
+    {
+        [Column(ordinal: "0", name: "Label")]
+        public float Sentiment;
+        [Column(ordinal: "1")]
+        public string Tweet;
+    }
+
+    public class SentimentPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public bool Prediction { get; set; }
+
+        [ColumnName("Probability")]
+        public float Probability { get; set; }
+
+        [ColumnName("Score")]
+        public float Score { get; set; }
+    }
+}
